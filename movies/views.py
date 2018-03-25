cacda09b61033bb0c18bcdd4c31bcf60371c2217
Krzysiekjk00 <krzysztof.kobialka90@gmail.com>
2018-03-25 15:39:10 +0200

    Zadanie 8

--- movies/views.py
+++ movies/views.py
@@ -2,6 +2,7 @@ from django.http import Http404
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -34,3 +35,16 @@ class MovieView(APIView):
         movie = self.get_object(id)
         serializer = MovieSerializer(movie, context={'request': request})
         return Response(serializer.data)
+
+    def delete(self, request, id, format=None):
+        movie = self.get_object(id)
+        movie.delete()
+        return Response(status=status.Http_204_NO_CONTENT)
+
+    def put(self, request, id, format=None):
+        movie = self.get_object(id)
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
